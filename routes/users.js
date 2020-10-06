06b0c4065026dcdff5457e36194a6b856fd70413
Kleber Castro <kleber.cristian@hotmail.com>
2020-10-06 17:28:27 -0300

    c1: add restify ok

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var assert = require('assert');
+var clients = require('restify-clients');
 var router = express.Router();
+
+var client = clients.createJsonClient({
+  url: 'http://192.168.0.101:8080',
+  version: '~1.0'
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  client.get('/users', function (err, request, response, obj) {
+    assert.ifError(err);
+    res.end(JSON.stringify(obj, null, 2));
+    
+  });
 });
 
 module.exports = router;
